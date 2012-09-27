b4670176344e981aa06bae8480a1077563fd2819
Eugene Wee <eugene@42ventures.com.sg>
2012-09-27 13:46:48 +0800

    Added MailChimpError exception.

--- mailchimp/__init__.py
+++ mailchimp/__init__.py
@@ -1,8 +1,9 @@
-from api import MailChimp
+from api import MailChimp, MailChimpError
 # Silence potential warnings from static analysis tools:
 assert MailChimp
+assert MailChimpError
 
-__author__ = "John-Kim Murphy"
+__author__ = "John-Kim Murphy, Eugene Wee"
 __copyright__ = ("Copyright 2010, John-Kim Murphy; "
                  "Copyright 2012, 42 Ventures Pte Ltd")
 __credits__ = ["John-Kim Murphy", "Eugene Wee"]
--- mailchimp/api.py
+++ mailchimp/api.py
@@ -6,6 +6,31 @@ except ImportError:
     import json
 
 
+class MailChimpError(Exception):
+    def __init__(self, error, code=None):
+        self.error = error
+        self.code = code
+        if self.code is not None:
+            try:
+                self.code = int(self.code)
+            except ValueError:
+                pass
+
+    def __unicode__(self):
+        if self.code is None:
+            message = self.error
+        else:
+            message = u"{error} ({code})".format(error=self.error,
+                                                 code=self.code)
+        return u"MailChimpError({message})".format(message=message)
+
+    def __str__(self):
+        return unicode(self).encode("utf8")
+
+    def __repr__(self):
+        return str(self)
+
+
 class MailChimp(object):
     def __init__(self, apikey='', extra_params={}):
         """
@@ -28,10 +53,17 @@ class MailChimp(object):
 
         post_data = urllib2.quote(json.dumps(all_params))
         headers = {"Content-Type": "application/json"}
-        request = urllib2.Request(url, post_data, headers)
-        response = urllib2.urlopen(request)
+        try:
+            request = urllib2.Request(url, post_data, headers)
+            response = urllib2.urlopen(request)
+            response_content = response.read()
+        except urllib2.HTTPError as e:
+            raise MailChimpError("HTTP {code}".format(code=e.code))
 
-        return json.loads(response.read())
+        result = json.loads(response_content)
+        if isinstance(result, dict) and "code" in result and "error" in result:
+            raise MailChimpError(error=result["error"], code=result["code"])
+        return result
 
     def __getattr__(self, method_name):
 
